ee632bb88e692b4f2b8c7ca37ee436e2115b05ab
Volodya1993 <volodag651@gmail.com>
2023-08-25 11:37:48 +0400

    homework

---
+++ src/chapter3/Homework.java
@@ -0,0 +1,10 @@
+package chapter3;
+
+public class Homework {
+    public static void main(String[] args) {
+        for (int i = 0; i < 6; i++) {
+            System.out.println("******");
+
+        }
+    }
+}
