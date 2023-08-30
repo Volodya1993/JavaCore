a82280c4b3b95eef85b2696fc426dc06a40e64dd
Volodya1993 <volodag651@gmail.com>
2023-08-30 10:18:08 +0400

    homework1

--- src/chapter3/CharExample3.java
+++ src/chapter3/CharExample3.java
@@ -3,11 +3,11 @@ package chapter3;
 public class CharExample3 {
     public static void main(String[] args) {
 
-        char[] chars = {'*', '*', '*', '*', '*'};
-        char[] chars1 = {'*', '*', '*', '*',};
-        char[] chars2 = {'*', '*', '*'};
-        char[] chars3 = {'*', '*'};
-        char[] chars4 = {'*'};
+        char[] chars = { ' ', ' ', ' ', ' ', '*'};
+        char[] chars1 = { ' ', ' ',' ', '*', '*'};
+        char[] chars2 = {' ',' ', '*', '*', '*'};
+        char[] chars3 = {' ','*', '*', '*', '*'};
+        char[] chars4 = {'*', '*', '*', '*', '*'};
         System.out.println(chars);
         ;
         System.out.println(chars1);
---
+++ src/exercises/DrawingsExample.java
@@ -0,0 +1,63 @@
+package exercises;
+
+public class DrawingsExample {
+    public static void main(String[] args) {
+        String name = "*";
+        String name2 = "**";
+        String name3 = "***";
+        String name4 = "****";
+        String name5 = "*****";
+        for (int i = 0; i < 1; i++) {
+            System.out.println(name + '\n' + name2 + '\n' + name3 + '\n' + name4 + '\n' + name5);
+
+        }
+        System.out.println();
+        System.out.println();
+
+      //  for (int i = 0; i < 1; i++) {
+            System.out.println("*****");
+            System.out.println("****");
+            System.out.println("***");
+            System.out.println("**");
+            System.out.println("*");
+            System.out.println();
+            System.out.println();
+            char[] chars = {' ', ' ', ' ', ' ', '*'};
+            char[] chars1 = {' ', ' ', ' ', '*', '*'};
+            char[] chars2 = {' ', ' ', '*', '*', '*'};
+            char[] chars3 = {' ', '*', '*', '*', '*'};
+            char[] chars4 = {'*', '*', '*', '*', '*'};
+            System.out.println(chars);
+            System.out.println(chars1);
+            System.out.println(chars2);
+            System.out.println(chars3);
+            System.out.println(chars4);
+            System.out.println();
+            System.out.println();
+            String a, b, c, d, e;
+            a = "*****";
+            b = " ****";
+            c = "  ***";
+            d = "   **";
+            e = "    *";
+            for (int j = 0; j < 1; j++)
+                System.out.println(a + '\n' + b + '\n' + c + '\n' + d + '\n' + e);
+            System.out.println();
+            System.out.println();
+
+
+
+            }
+
+        }
+
+
+
+
+
+
+
+
+
+
+
